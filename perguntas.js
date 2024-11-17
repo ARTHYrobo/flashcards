5704e71e18a1107f1769201fcba341e6410fdada
ARTHYrobo <133902076+ARTHYrobo@users.noreply.github.com>
2024-11-17 15:49:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,76 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O Html é uma linguagem de marcação'
+)
+
+criaCartao(
+    'Animes',
+    'Em que ano o estudio Ghibli foi lançado?',
+    '15 de junho de 1985'
+)
+
+
+
+criaCartao(
+    'Animes',
+    'Melhor personagem de Demon Slayer',
+    'Rengoku'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tnoevry Grvkrven, niryvab*/
